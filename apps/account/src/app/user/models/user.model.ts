1adccd79839d96310596d382547fc7a520e72039
Andrey <andreikrauchanka1994@gmail.com>
2025-02-08 15:52:58 +0300

    feat: create User model

---
+++ apps/account/src/app/user/models/user.model.ts
@@ -0,0 +1,17 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { IUser, UserRole } from '@purple/interfaces';
+import { Document } from 'mongoose';
+
+@Schema()
+export class User extends Document<IUser['_id']> implements IUser {
+  @Prop()
+  displayName?: string;
+  @Prop({ required: true })
+  email: string;
+  @Prop({ required: true })
+  passwordHash: string;
+  @Prop({ required: true, enum: UserRole, type: String, default: UserRole.Student })
+  role: UserRole;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
